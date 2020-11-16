730565df584741dceb7e325c502e4e99189e22e5
KJ Osenenko <karljosenenko@gmail.com>
2020-11-16 16:19:23 -0700

    commit tree c0b9d7ff3e150dd4db4953a9f66cc5012d1c7a0f

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :detector
+
+    def initialize(detector)
+        @detector = detector
+    end
+
+    def match(array)
+        matches = []
+        array.map do |word|
+            if word.chars.sort == @detector.chars.sort
+                matches << word
+            end
+        end
+        matches
+    end
+end 
